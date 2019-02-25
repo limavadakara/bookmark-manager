83e4635a1260a691ac9d885947fa0713b08088f5
Lima Vadakara <lima.vadakara@gmail.com>
2019-02-25 15:40:19 +0000

    display bookmarks without classes

--- bookmark_web.rb
+++ bookmark_web.rb
@@ -1,9 +1,14 @@
 require 'sinatra/base'
 
 class BookmarkWeb < Sinatra::Base
+  $bookmarks = [{name: "Bookmark1"}, {name: "Bookmark2"}]
+  get '/bookmarks' do
+      @list = []
+      $bookmarks.each do |bookmark|
+        @list << bookmark[:name]
+      end
 
-  get '/' do
-    erb :index
+    erb :bookmarks
   end
 
  run! if app_file == $0
--- spec/features/view_bookmarks_spec.rb
+++ spec/features/view_bookmarks_spec.rb
@@ -1,6 +1,8 @@
+
 feature 'As a user I want to ' do
   scenario 'see all my bookmarks' do
-    visit "/"
+    visit "/bookmarks"
     expect(page).to have_content "Here are your bookmarks"
+    expect(page).to have_content "Bookmark1" and "Bookmark2"
   end
 end
