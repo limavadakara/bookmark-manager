79884330d66a51d8bf5b9215563ebea37a08a926
matt <mwhitaker1006@gmail.com>
2019-02-27 14:48:22 +0000

    added failing test to check for presence of link on bookmarks view page

--- spec/features/add_new_bookmark_spec.rb
+++ spec/features/add_new_bookmark_spec.rb
@@ -7,4 +7,13 @@ feature "Add new bookmark" do
     click_button('Add')
     expect(page).to have_content('www.bbc.co.uk')
   end
+
+  scenario "Adds www.bbc.co.uk to bookmark list" do
+    visit('/')
+    click_button('Add Bookmark')
+    fill_in("url", with: 'www.guardian.co.uk')
+    fill_in("title", with: 'Guardian website')
+    click_button('Add')
+    expect(page).to have_link 'Guardian website', href: 'www.guardian.co.uk'
+  end
 end
